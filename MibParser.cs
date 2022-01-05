657e3dfa27543874c0bcd02bcbb4a88d77d94946
Todor Manev <tmanev@kvarta.net>
2022-01-05 16:29:09 +0200

    Changed project name to Kvartasoft.Snmp.MibParser

--- MibParser.cs
+++ MibParser.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoreMibParser
+namespace Kvartasoft.Snmp.MibParser
 {
 
     public enum SnmpItemType
--- MibTable.cs
+++ MibTable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CoreMibParser
+namespace Kvartasoft.Snmp.MibParser
 {
     public class MibTable
     {
